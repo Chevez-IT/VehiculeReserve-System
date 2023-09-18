24ddb3a9dc3043230f36ab2c253add73a307694d
Chevez-IT <chevez.diego.it@gmail.com>
2023-09-18 11:43:22 -0600

    solucion de un bug

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Te logueaste') }}
                 </div>
             </div>
         </div>
